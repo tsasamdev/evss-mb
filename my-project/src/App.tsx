7ceba9e2d6157ba906a1502333455da93adb09fc
artsidev <tsiory.arisoa@gmail.com>
2025-03-11 21:35:55 +0100

    Added vercel analytics

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MassBalanceChart from "./MassBalanceChart";
+import { Analytics } from "@vercel/analytics/react"
 
 const App: React.FC = () => {
   return (
     <div >
       <MassBalanceChart />
+      <Analytics />
     </div>
     
   );
